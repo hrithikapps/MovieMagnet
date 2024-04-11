336961967cbce6b3f7a243f7dfe84b5839c1ade9
hrithikapps <hrithikk2765@gmail.com>
2024-04-11 17:03:19 +0530

    redux store setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
+import { fetchDataFromApi } from "./utils/api";
+import { getApiConfiguration } from "./store/homeSlice";
 
 function App() {
-  return <>App</>;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const url = useSelector((store) => {
+    return store.home.url;
+  });
+
+  console.log("url", url);
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      dispatch(getApiConfiguration(res));
+    });
+  };
+
+  return (
+    <div className="App">
+      App
+      {url?.total_pages}
+    </div>
+  );
 }
 
 export default App;
